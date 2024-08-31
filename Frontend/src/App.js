9d474283f1feded089ce241f003500cb866cc9a0
yadavabhi02 <abhishek02082002@gmail.com>
2024-08-31 17:28:53 +0530

    Initial Commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import io from "socket.io-client";
+import ClientRoom from "./ClientRoom";
+import JoinCreateRoom from "./JoinCreateRoom";
+import Chatbar from "./Chatbar";
+import Room from "./Room";
+import Sidebar from "./Sidebar";
+import "./style.css";
+
+const server = "http://localhost:5000";
+const connectionOptions = {
+  "force new connection": true,
+  reconnectionAttempts: "Infinity",
+  timeout: 10000,
+  transports: ["websocket"],
+};
+
+const socket = io(server, connectionOptions);
+
+const App = () => {
+  const [userNo, setUserNo] = useState(0);
+  const [roomJoined, setRoomJoined] = useState(false);
+  const [user, setUser] = useState({});
+  const [users, setUsers] = useState([]);
+  const [openedChatTab, setOpenedChatTab] = useState(false);
+
+  const uuid = () => {
+    var S4 = () => {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (
+      S4() +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      S4() +
+      S4()
+    );
+  };
+
+  useEffect(() => {
+    if (roomJoined) {
+      socket.emit("user-joined", user);
+    }
+  }, [roomJoined]);
+
+  return (
+    <div className="home">
+      <ToastContainer />
+      {roomJoined ? (
+        <>
+          <Sidebar users={users} user={user} socket={socket} />  
+
+          <button
+        type="button"
+        className="btn btn-primary"
+        style={{
+          display: "block",
+          position: "absolute",
+          top: "5%",
+          left: "10%",
+          height: "40px",
+          width: "100px",
+        }}
+        onClick={() => setOpenedChatTab(true)}
+      >
+        Chats
+      </button>
+
+          {openedChatTab && ( <Chatbar setOpenedChatTab={setOpenedChatTab} socket={socket} /> )}
+
+          {user.presenter ? (
+            <Room
+              userNo={userNo}
+              user={user}
+              socket={socket}
+              setUsers={setUsers}
+              setUserNo={setUserNo}
+            />
+          ) : (     
+            <ClientRoom
+              userNo={userNo}
+              user={user}
+              socket={socket}
+              setUsers={setUsers}
+              setUserNo={setUserNo}
+            />
+          )}
+        </>
+      ) : (
+        <JoinCreateRoom
+          uuid={uuid}
+          setRoomJoined={setRoomJoined}
+          setUser={setUser}
+        />
+      )}
+    </div>
+  );
+};
+
+
+  
+export default App;
---
+++ README.md
@@ -0,0 +1,47 @@
+
+
+ # Real Time Whiteboard Sharing App 
+
+
+
+## Overview
+
+The Real-Time Whiteboard Sharing App is a collaborative tool that allows users to share a whiteboard in real-time. The app is built using ReactJS, Rough.js, Node.js, and Socket.IO. It provides a digital canvas where the presenter can draw, erase, and perform other actions, while viewers can observe these changes in real-time. Additionally, a group chat feature allows participants to communicate during the session.
+
+
+## Features
+
+1.Whiteboard Canvas:
+
+Drawing: The presenter can draw using a variety of colors and pen sizes.
+Erasing: The presenter can erase specific parts of the drawing.
+Undo/Redo: The presenter can undo or redo their actions on the canvas.
+Real-Time Updates: Viewers can see the presenter's actions in real-time.
+
+2.Presenter and Viewer Roles:
+
+3.Presenter (Host): The user who initiates the session and has the ability to draw on the whiteboard.
+Viewers: Participants who can view the whiteboard but do not have drawing permissions.
+Group Chat:
+
+A text-based chat feature that allows all participants to communicate during the session.
+
+## Tech Stack
+
+Frontend: ReactJS, Rough.js
+Backend: Node.js
+Real-Time Communication: Socket.IO
+
+## Installation Steps
+Clone the repository:
+   git clone https://github.com/your-repo/real-time-whiteboard.git
+   cd real-time-whiteboard
+
+## Install dependencies:
+   npm install
+
+## Run the server:
+   npm start
+
+
+The app will typically run on http://localhost:3000 (or another port if specified).
